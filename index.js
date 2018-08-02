8d1f4812e09ec239324c8dbfdd995a894c0bdf11
Rachel Grier <rgrier104@gmail.com>
2018-08-02 01:39:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return `There is nobody waiting to be served!`
+  } else {
+    return `Currently serving ${katzDeliLine[0]}.`
+    katzDeliLine.pop()
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length != 0) {
+    for (var i=0; i < katzDeliLine.length; i++) {
+      katzDeliLine.push(` ${i+1}. ${katzDeliLine[i]}`)
+    }
+  return `The line is currently: ${katzDeliLine}`
+  } else {
+  return `The line is currently empty.`
+  }
+}
