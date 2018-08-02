6610c63df9d159463d757b8811c9a1ce7abe7a18
Rachel Grier <rgrier104@gmail.com>
2018-08-02 02:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
     for (var i=0; i < katzDeliLine.length; i++) {
       newLine.push(` ${i+1}. ${katzDeliLine[i]}`)
     }
-  return `The line is currently: ${katzDeliLine}`
+  return `The line is currently: ${newLine}`
   } else {
   return `The line is currently empty.`
   }
